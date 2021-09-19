ac91dacc3d0f1ace4269ba62ecccbd30122bf540
Vansh <vvanshg@gmail.com>
2021-09-19 18:05:07 +0530

    feat : added toolbar options

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,19 @@ import React , {useEffect , useRef , useCallback} from 'react'
 import Quill  from 'quill'
 import "quill/dist/quill.snow.css"
 
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"]
+]
+
 export default function TextEditor() {
    // const wrapRef = useRef();
     //problem with useEffect : sometimes wrapRef is not defined before render starts
@@ -19,7 +32,7 @@ export default function TextEditor() {
         //first line of return
         // so if we pass editor to that div then all the toolbars will be put in the div container.
         //rest check in inspect
-        new Quill(editor , {theme : "snow"})
+        new Quill(editor , {theme : "snow" , modules : {toolbar : TOOLBAR_OPTIONS}})
         // return () => {// for the cleanup of the old toolbars after each render
         //     wrapRef.innerHTML = "";
         //     // if we don't do this then the old toolbars will persist
